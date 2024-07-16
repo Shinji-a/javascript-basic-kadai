435359f8d63a6902fd276edb6fdc0fd6e7aa0348
Shinji-a <onemewbowwow@gmail.com>
2024-07-16 21:42:57 +0900

    ver 2

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,13 +1,13 @@
 let num;
-num = 5;
-if (num % 3 == 0) {
-    console.log('3の倍数です');
+num = 4540;
+if (num % 15 == 0) {
+    console.log('3と5の倍数です');
 }
 else if (num % 5 == 0){
     console.log('5の倍数です');
 }
-else if (num % 15 == 0){
-    console.log('3と5の倍数です');
+else if (num % 3 == 0){
+    console.log('3の倍数です');
 }
 else {
     console.log(num)
