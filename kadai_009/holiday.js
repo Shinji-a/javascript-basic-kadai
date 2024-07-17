6b1ca2a5fab9ad17d76718773aa429ebf20a1841
Shinji-a <onemewbowwow@gmail.com>
2024-07-17 22:24:23 +0900

    Add files via upload

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,7 +1,12 @@
 const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
-let i = 0
-while (i < 17) {
-        for (i = 0; i <= 16; i += 1) {
+let num = 0
+  while (num < 17) {
+    num += 1
+    console.log(holidays[num])
+
+  }
+
+  let i = 0
+for (i = 0; i <= 16; i += 1) {
         console.log(holidays[i]);
   }
-}
