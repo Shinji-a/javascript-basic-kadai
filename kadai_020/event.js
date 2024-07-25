8e62d35aeb581feacdfc94180fca7231c501254d
Shinji-a <onemewbowwow@gmail.com>
2024-07-25 14:35:20 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const Cbtn = document.getElementById('btn');
+ Cbtn.addEventListener('click', 
+    function Cbtni() {
+    const CbtnB = document.getElementById('text');
+    CbtnB.textContent = 'ボタンをクリックしました';
+  });
